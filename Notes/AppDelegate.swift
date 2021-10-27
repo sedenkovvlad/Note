95001803290f3eefaf81e85976f83c7c59a65617
SedenkovVlad <vladsedenkov@gmail.com>
2021-10-27 13:41:43 +0300

    Add edit mode (delete all notes and delete selected notes)

--- Notes/AppDelegate.swift
+++ Notes/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UINavigationBar.appearance().tintColor = .orange
+        UIToolbar.appearance().tintColor = .orange
         return true
     }
 
--- Notes/Screen/NoteViewController.swift
+++ Notes/Screen/NoteViewController.swift
@@ -13,12 +13,18 @@ class NoteViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     private lazy var notes = [Note]()
     private lazy var addButton = UIButton()
+    private lazy var editButton = UIBarButtonItem()
+    private lazy var cancelButton = UIBarButtonItem()
+    private lazy var spacerButton = UIBarButtonItem()
+    private lazy var deleteAllButton = UIBarButtonItem()
+    private lazy var deleteButton = UIBarButtonItem()
     
     //MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureAddButton()
         updateData()
+        configureNavigationBar()
     }
 }
 
@@ -41,9 +47,19 @@ extension NoteViewController: UITableViewDataSource {
 //MARK: TableViewDelegate
 extension NoteViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        pushCreateController()
-        tableView.deselectRow(at: indexPath, animated: true)
+        if tableView.isEditing {
+            toolbarItems = [spacerButton, deleteButton]
+        } else {
+            pushCreateController()
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if tableView.indexPathForSelectedRow == nil || tableView.indexPathForSelectedRow!.isEmpty {
+            toolbarItems = [spacerButton, deleteAllButton]
+        }
+    }
+    
 }
 
 //MARK: Navigation
@@ -62,47 +78,19 @@ extension NoteViewController {
 extension NoteViewController: CreateViewControllerDelegate {
     func saveNotes(didFinishAdditing note: Note) {
         self.notes.append(note)
+        sortNotes()
         saveData()
     }
     func saveNotes(didFinishEditing note: Note) {
         if let index = notes.firstIndex(of: note) {
             let indexPath = IndexPath(row: index, section: 0)
             notes[indexPath.row] = note
+            sortNotes()
         }
         saveData()
     }
 }
 
-//MARK: Helpers
-extension NoteViewController {
-    //text split
-    func getTitleText(split: [Substring]) -> String {
-        if split.count >= 1 {
-            return String(split[0])
-        }
-        return "New Note"
-    }
-    
-    func getSubtitleText(split: [Substring]) -> String {
-        if split.count >= 2 {
-            return String(split[1])
-        }
-        return "No Subtitle text"
-    }
-    //date formatter
-    func dateFormat(from date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .none
-        formatter.locale = Locale(identifier: "ru_RU")
-        return formatter.string(from: date)
-    }
-    
-    func sortNotes() {
-        notes.sort { $0.date > $1.date }
-    }
-    
-}
 
 //MARK: StorageManager
 extension NoteViewController {
@@ -138,5 +126,99 @@ extension NoteViewController {
         shimmerView.shimmerDirection = .up
         shimmerView.isShimmering = true
     }
+    
+    func configureNavigationBar() {
+        //navigation bar
+        editButton = UIBarButtonItem(image: UIImage(systemName: "square.and.pencil"), style: .plain, target: self, action: #selector(enterEditingMode))
+        cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector((cancelEditingMode)))
+        navigationItem.rightBarButtonItems = [editButton]
+        //toolbar
+        spacerButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        deleteAllButton = UIBarButtonItem(title: "Delete All", style: .plain, target: self, action: #selector(deleteAllNotes))
+        deleteButton = UIBarButtonItem(image: UIImage(systemName: "trash"), style: .plain, target: self, action: #selector(deleteSelectedNotes))
+        tableView.allowsMultipleSelectionDuringEditing = true
+    }
 }
 
+
+//MARK: EditMode
+
+extension NoteViewController {
+    @objc private func enterEditingMode() {
+        navigationItem.rightBarButtonItems = [cancelButton]
+        navigationController?.isToolbarHidden = false
+        toolbarItems = [spacerButton, deleteAllButton]
+        addButton.removeFromSuperview()
+        tableView.setEditing(true, animated: true)
+    }
+    
+    @objc private func cancelEditingMode() {
+        navigationItem.rightBarButtonItems = [editButton]
+        navigationController?.isToolbarHidden = true
+        configureAddButton()
+        tableView.setEditing(false, animated: true)
+    }
+    
+    @objc private func deleteAllNotes() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let deleteAction = UIAlertAction(title: "Delete All", style: .destructive) { [weak self] _ in
+            self?.notes.removeAll()
+            self?.saveData()
+            self?.cancelEditingMode()
+        }
+        
+        alert.addAction(cancelAction)
+        alert.addAction(deleteAction)
+        present(alert, animated: true)
+    }
+    
+    @objc private func deleteSelectedNotes() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let deleteAction = UIAlertAction(title: "Delete Selected", style: .destructive) { [weak self] _ in
+            if let rows = self?.tableView.indexPathsForSelectedRows {
+                for i in rows {
+                    self?.notes.remove(at: i.startIndex)
+                }
+            }
+            self?.cancelEditingMode()
+            self?.saveData()
+        }
+        
+        alert.addAction(cancelAction)
+        alert.addAction(deleteAction)
+        present(alert, animated: true)
+    }
+}
+
+//MARK: Helpers
+extension NoteViewController {
+    //text split
+    func getTitleText(split: [Substring]) -> String {
+        if split.count >= 1 {
+            return String(split[0])
+        }
+        return "New Note"
+    }
+    
+    func getSubtitleText(split: [Substring]) -> String {
+        if split.count >= 2 {
+            return String(split[1])
+        }
+        return "No Subtitle text"
+    }
+    //date formatter
+    func dateFormat(from date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        formatter.locale = Locale(identifier: "ru_RU")
+        return formatter.string(from: date)
+    }
+    
+    func sortNotes() {
+        notes.sort { $0.date > $1.date }
+    }
+    
+}
